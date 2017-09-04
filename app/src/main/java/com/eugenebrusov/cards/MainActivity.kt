64e8944bee7c2276510cf8aa6bf627aa1912c39b
Eugene Brusov <eugenebrusov@gmail.com>
2017-09-04 12:18:24 +0200

    RecyclerView & Adapter

--- app/src/main/java/com/eugenebrusov/cards/MainActivity.kt
+++ app/src/main/java/com/eugenebrusov/cards/MainActivity.kt
@@ -2,11 +2,17 @@ package com.eugenebrusov.cards
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.recycler_view
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recycler_view.setHasFixedSize(true)
+        recycler_view.layoutManager = LinearLayoutManager(this)
+        recycler_view.adapter = MainAdapter()
     }
 }
---
+++ app/src/main/java/com/eugenebrusov/cards/MainAdapter.kt
@@ -0,0 +1,26 @@
+package com.eugenebrusov.cards
+
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.RecyclerView.ViewHolder
+import android.view.LayoutInflater
+import android.view.ViewGroup
+
+/**
+ * Created by Eugene Brusov on 9/4/17.
+ */
+class MainAdapter : RecyclerView.Adapter<ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
+        val layout = LayoutInflater.
+                from(parent?.context).
+                inflate(R.layout.item, parent, false)
+        return object : ViewHolder(layout) { /** empty implementation */ }
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
+        /** empty implementation */
+    }
+
+    override fun getItemCount(): Int {
+        return 0
+    }
+}
